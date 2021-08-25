767ea5d89430a4b7f91d0dd59ab4713b0a310f21
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-08-25 13:37:55 +0900

    Update research_type.py

--- research_type.py
+++ research_type.py
@@ -44,3 +44,45 @@ print(df.head(50))
 df['Order_Demand']=df['Order_Demand'].str.replace('(',"", regex=True)
 df['Order_Demand']=df['Order_Demand'].str.replace(')',"", regex=True)
 df['Order_Demand'] = df['Order_Demand'].astype('int64')
+
+
+def scale_module(df, targetName):
+    y=df[targetName]
+    X=df.drop([targetName],1)
+    X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+    # Normalization with 4 Scaling methods
+    maxAbsScaler = preprocessing.MaxAbsScaler()
+    minmaxScaler = preprocessing.MinMaxScaler()
+    robustScaler = preprocessing.RobustScaler()
+    standardScaler = preprocessing.StandardScaler()
+
+    df_maxAbs_scaled_train = maxAbsScaler.fit_transform(X_train)
+    df_maxAbs_scaled_train = pd.DataFrame(df_maxAbs_scaled_train, columns=X_train.columns)
+    df_maxAbs_scaled_test = maxAbsScaler.fit_transform(X_test)
+    df_maxAbs_scaled_test = pd.DataFrame(df_maxAbs_scaled_test, columns=X_test.columns)
+
+    df_minMax_scaled_train = minmaxScaler.fit_transform(X_train)
+    df_minMax_scaled_train = pd.DataFrame(df_minMax_scaled_train, columns=X_train.columns)
+    df_minMax_scaled_test = minmaxScaler.fit_transform(X_test)
+    df_minMax_scaled_test = pd.DataFrame(df_minMax_scaled_test, columns=X_test.columns)
+
+    df_robust_scaled_train = robustScaler.fit_transform(X_train)
+    df_robust_scaled_train = pd.DataFrame(df_robust_scaled_train, columns=X_train.columns)
+    df_robust_scaled_test = robustScaler.fit_transform(X_test)
+    df_robust_scaled_test = pd.DataFrame(df_robust_scaled_test, columns=X_test.columns)
+
+    df_standard_scaled_train = standardScaler.fit_transform(X_train)
+    df_standard_scaled_train = pd.DataFrame(df_standard_scaled_train, columns=X_train.columns)
+    df_standard_scaled_test = standardScaler.fit_transform(X_test)
+    df_standard_scaled_test = pd.DataFrame(df_standard_scaled_test, columns=X_test.columns)
+
+    # Alogirthm
+    print("\n------------------------- Using maxAbs scaled dataset -------------------------")
+    max_score_maxAbs = algorithm_module(df_maxAbs_scaled_train, df_maxAbs_scaled_test, y_train, y_test)
+    print("\n------------------------- Using minMax scaled dataset -------------------------")
+    max_score_minMax = algorithm_module(df_minMax_scaled_train, df_minMax_scaled_test, y_train, y_test)
+    print("\n------------------------- Using robust scaled dataset -------------------------")
+    max_score_robust = algorithm_module(df_robust_scaled_train, df_robust_scaled_test, y_train, y_test)
+    print("\n------------------------- Using standard scaled dataset -------------------------")
+    max_score_standard = algorithm_module(df_standard_scaled_train, df_standard_scaled_test, y_train, y_test)
