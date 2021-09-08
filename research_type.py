4dd0c1138dd34df7c921f9994643e55ab055c3e7
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-09-08 13:51:34 +0900

    Update research_type.py

--- research_type.py
+++ research_type.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
 import seaborn as sn
 from sklearn.preprocessing import StandardScaler
+from sklean.model_selection import cross_val_score,cross_val_predict
+from sklearn.model import metrics
 
 from scipy.stats import norm, skew #Import Norm and skew for some statistics
 from scipy import stats #Import stats
@@ -67,43 +69,64 @@ df['Order_Demand']=df['Order_Demand'].str.replace(')',"", regex=True)
 df['Order_Demand'] = df['Order_Demand'].astype('int64')
 
 
-def scale_module(df, targetName):
+def maxAbs_scale_module(df, targetName):
     y=df[targetName]
     X=df.drop([targetName],1)
     X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=0)
 
-    # Normalization with 4 Scaling methods
-    maxAbsScaler = preprocessing.MaxAbsScaler()
-    minmaxScaler = preprocessing.MinMaxScaler()
-    robustScaler = preprocessing.RobustScaler()
-    standardScaler = preprocessing.StandardScaler()
-
+    maxAbsScaler = preprocessing.MaxAbsScaler() 
     df_maxAbs_scaled_train = maxAbsScaler.fit_transform(X_train)
     df_maxAbs_scaled_train = pd.DataFrame(df_maxAbs_scaled_train, columns=X_train.columns)
     df_maxAbs_scaled_test = maxAbsScaler.fit_transform(X_test)
     df_maxAbs_scaled_test = pd.DataFrame(df_maxAbs_scaled_test, columns=X_test.columns)
 
+    
+   
+    
+def minmax_scale_module(df, targetName):
+    y=df[targetName]
+    X=df.drop([targetName],1)
+    X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+    minmaxScaler = preprocessing.MinMaxScaler()
     df_minMax_scaled_train = minmaxScaler.fit_transform(X_train)
     df_minMax_scaled_train = pd.DataFrame(df_minMax_scaled_train, columns=X_train.columns)
     df_minMax_scaled_test = minmaxScaler.fit_transform(X_test)
     df_minMax_scaled_test = pd.DataFrame(df_minMax_scaled_test, columns=X_test.columns)
 
+def robust_scale_module(df, targetName):
+    y=df[targetName]
+    X=df.drop([targetName],1)
+    X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+    robustScaler = preprocessing.RobustScaler()
     df_robust_scaled_train = robustScaler.fit_transform(X_train)
     df_robust_scaled_train = pd.DataFrame(df_robust_scaled_train, columns=X_train.columns)
     df_robust_scaled_test = robustScaler.fit_transform(X_test)
     df_robust_scaled_test = pd.DataFrame(df_robust_scaled_test, columns=X_test.columns)
 
+def standard_scale_module(df, targetName):
+    y=df[targetName]
+    X=df.drop([targetName],1)
+    X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+    standardScaler = preprocessing.StandardScaler()
     df_standard_scaled_train = standardScaler.fit_transform(X_train)
     df_standard_scaled_train = pd.DataFrame(df_standard_scaled_train, columns=X_train.columns)
     df_standard_scaled_test = standardScaler.fit_transform(X_test)
     df_standard_scaled_test = pd.DataFrame(df_standard_scaled_test, columns=X_test.columns)
 
-    # Alogirthm
-    print("\n------------------------- Using maxAbs scaled dataset -------------------------")
-    max_score_maxAbs = algorithm_module(df_maxAbs_scaled_train, df_maxAbs_scaled_test, y_train, y_test)
-    print("\n------------------------- Using minMax scaled dataset -------------------------")
-    max_score_minMax = algorithm_module(df_minMax_scaled_train, df_minMax_scaled_test, y_train, y_test)
-    print("\n------------------------- Using robust scaled dataset -------------------------")
-    max_score_robust = algorithm_module(df_robust_scaled_train, df_robust_scaled_test, y_train, y_test)
-    print("\n------------------------- Using standard scaled dataset -------------------------")
-    max_score_standard = algorithm_module(df_standard_scaled_train, df_standard_scaled_test, y_train, y_test)
+df_ordinal_maxAbs = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_ordinal_minmax = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_ordinal_robust = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_ordiinal_standard = maxAbs_scale_module(df_ordinal,'Order_Demand')
+
+df_oneHot_maxAbs = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_oneHot_minmax = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_oneHot_robust = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_oneHot_standard = maxAbs_scale_module(df_ordinal,'Order_Demand')
+
+df_label_maxAbs = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_label_minmax = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_label_robust = maxAbs_scale_module(df_ordinal,'Order_Demand')
+df_label_standard = maxAbs_scale_module(df_ordinal,'Order_Demand')
