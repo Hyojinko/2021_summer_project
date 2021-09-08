02f857a31b5977e9dcfa07cb0cc032fbb965f9d1
Hyojinko <62235186+Hyojinko@users.noreply.github.com>
2021-09-08 14:34:01 +0900

    Update research_type.py

--- research_type.py
+++ research_type.py
@@ -32,79 +32,39 @@ def ordinalEncode_category(df, str):
     df[str] = pd.DataFrame(ordinalEncoder.transform(X))
 
 
-df_ordinal = df.copy()
 
 
-ordinalEncode_category(df_ordinal, "Product_Code")
-ordinalEncode_category(df_ordinal, "Warehouse")
-ordinalEncode_category(df_ordinal, "Product_Category")
-ordinalEncode_category(df_ordinal, "Date")
+
+ordinalEncode_category(df, "Product_Code")
+ordinalEncode_category(df, "Warehouse")
+ordinalEncode_category(df, "Product_Category")
+ordinalEncode_category(df, "Date")
 
 # Since the "()" has been removed , Now i Will change the data type.
 df['Order_Demand'] = df['Order_Demand'].str.replace('(', "", regex=True)
 df['Order_Demand'] = df['Order_Demand'].str.replace(')', "", regex=True)
 df['Order_Demand'] = df['Order_Demand'].astype('int64')
 
-
-def maxAbs_scale_module(df, targetName):
-    y = df[targetName]
-    X = df.drop([targetName], 1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-    maxAbsScaler = preprocessing.MaxAbsScaler()
-    df_maxAbs_scaled_train = maxAbsScaler.fit_transform(X_train)
-    df_maxAbs_scaled_train = pd.DataFrame(df_maxAbs_scaled_train, columns=X_train.columns)
-    df_maxAbs_scaled_test = maxAbsScaler.fit_transform(X_test)
-    df_maxAbs_scaled_test = pd.DataFrame(df_maxAbs_scaled_test, columns=X_test.columns)
-
-
-def minmax_scale_module(df, targetName):
-    y = df[targetName]
-    X = df.drop([targetName], 1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-    minmaxScaler = preprocessing.MinMaxScaler()
-    df_minMax_scaled_train = minmaxScaler.fit_transform(X_train)
-    df_minMax_scaled_train = pd.DataFrame(df_minMax_scaled_train, columns=X_train.columns)
-    df_minMax_scaled_test = minmaxScaler.fit_transform(X_test)
-    df_minMax_scaled_test = pd.DataFrame(df_minMax_scaled_test, columns=X_test.columns)
-
-
-def robust_scale_module(df, targetName):
-    y = df[targetName]
-    X = df.drop([targetName], 1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
-    robustScaler = preprocessing.RobustScaler()
-    df_robust_scaled_train = robustScaler.fit_transform(X_train)
-    df_robust_scaled_train = pd.DataFrame(df_robust_scaled_train, columns=X_train.columns)
-    df_robust_scaled_test = robustScaler.fit_transform(X_test)
-    df_robust_scaled_test = pd.DataFrame(df_robust_scaled_test, columns=X_test.columns)
-
-
-def standard_scale_module(df, targetName):
-    y = df[targetName]
-    X = df.drop([targetName], 1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-
+       
+def scale_module(df):
     standardScaler = preprocessing.StandardScaler()
-    df_standard_scaled_train = standardScaler.fit_transform(X_train)
-    df_standard_scaled_train = pd.DataFrame(df_standard_scaled_train, columns=X_train.columns)
-    df_standard_scaled_test = standardScaler.fit_transform(X_test)
-    df_standard_scaled_test = pd.DataFrame(df_standard_scaled_test, columns=X_test.columns)
+    df_standard_scaled = standardScaler.fit_transform(df)
+    df_standard_scaled = pd.DataFrame(df_standard_scaled, columns = df.columns)
+       
+
+scale_module(df)
+print(df.head(10))
+y=df['Order_Demand']
+X=df.drop(['Order_Demand'],1)
+X_train, X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+#Grouping demand to identify datewise sales
+demand_grouped = df.groupby(['Product_Code','Date']).agg(total_sale=('Order_Demand','sum')).reset_index()
+#calculating average and standard deviation
+cv_data = demand_grouped.groupby('Product_Code').agg(average=('total_sale','mean'),sd=('total_sale','std')).reset_index()
+#calculating CV squared
+cv_data['cv_sqr']=(cv_data['sd']/cv_data['average'])**2
+print(cv_data.head(10))
 
 
-df_ordinal_maxAbs = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_ordinal_minmax = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_ordinal_robust = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_ordiinal_standard = maxAbs_scale_module(df_ordinal, 'Order_Demand')
 
-df_oneHot_maxAbs = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_oneHot_minmax = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_oneHot_robust = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_oneHot_standard = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-
-df_label_maxAbs = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_label_minmax = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_label_robust = maxAbs_scale_module(df_ordinal, 'Order_Demand')
-df_label_standard = maxAbs_scale_module(df_ordinal, 'Order_Demand')
